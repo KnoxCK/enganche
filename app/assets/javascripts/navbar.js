320635edd06707f8f949f66aba31da892bc795b0
Clarissa Knox <knox.ck@gmail.com>
2018-02-27 12:46:57 +0000

    done

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,20 +1,20 @@
 $(document).ready(function(){
   $(".navbar-enganche-link").click(function(){
     $(".navbar-enganche-link").removeClass("active");
-    if ($(this).prop('id') === 'playbook-nav') {
+    if ($(this).prop('id') === 'about-nav') {
+      $.smoothScroll({
+        scrollTarget: $(this).attr('href').substr(1),
+        offset: -225,
+        speed: 1500
+      });
+    } 
+    else if ($(this).prop('id') === 'sponsorship-nav') {
       $.smoothScroll({
         scrollTarget: $(this).attr('href').substr(1),
         offset: -250,
         speed: 1500
       });
     } 
-    // else if ($(this).prop('id') === 'about-nav') {
-    //   $.smoothScroll({
-    //     scrollTarget: $(this).attr('href').substr(1),
-    //     offset: -65,
-    //     speed: 1500
-    //   });
-    // } 
     else {
       $.smoothScroll({
         scrollTarget: $(this).attr('href').substr(1),
@@ -28,8 +28,8 @@ $(document).ready(function(){
     var top = $(window).scrollTop();
     var bottom = top + $(window).height();
     var half = (bottom + top) / 2
+    var sponsorshipTop = $('#sponsorship').offset().top;
     var aboutTop = $('#about').offset().top;
-    var playbookTop = $('#playbook').offset().top;
     var whyTop = $('#why-enganche').offset().top;
     var contactTop = $('#contact').offset().top;
 
@@ -39,12 +39,12 @@ $(document).ready(function(){
     } else if (whyTop < half) {
       $(".navbar-enganche-link").removeClass("active");
       $('#why-enganche-nav').addClass('active');
-    } else if (playbookTop < half) {
-      $(".navbar-enganche-link").removeClass("active");
-      $('#playbook-nav').addClass('active');
     } else if (aboutTop < half) {
       $(".navbar-enganche-link").removeClass("active");
       $('#about-nav').addClass('active');
+    } else if (sponsorshipTop < half) {
+      $(".navbar-enganche-link").removeClass("active");
+      $('#sponsorship-nav').addClass('active');
       console.log('oi oi')
     } else {
       $(".navbar-enganche-link").removeClass("active");
@@ -54,7 +54,7 @@ $(document).ready(function(){
   $(".arrow").click(function(){
     $.smoothScroll({
       scrollTarget: $(this).attr('href').substr(1),
-      offset: -50,
+      offset: -400,
       speed: 1000
     });
   })
