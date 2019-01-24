84f04af14f09eab0fc955f7c9d81e2248a0a9691
Clarissa Knox <knox.ck@gmail.com>
2019-01-24 10:07:48 +0000

    last changes

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -8,24 +8,24 @@ $(document).ready(function(){
         speed: 1500
       });
     } 
+    else if ($(this).prop('id') === 'brands-nav') {
+      $.smoothScroll({
+        scrollTarget: $(this).attr('href').substr(1),
+        offset: -260,
+        speed: 1500
+      });
+    } 
     else if ($(this).prop('id') === 'sponsorship-nav') {
       $.smoothScroll({
         scrollTarget: $(this).attr('href').substr(1),
-        offset: -230,
+        offset: -275,
         speed: 1500
       });
-    } 
-    else if ($(this).prop('id') === 'why-enganche-nav') {
-      $.smoothScroll({
-        scrollTarget: $(this).attr('href').substr(1),
-        offset: -0,
-        speed: 1500
-      });
-    } 
+    }
     else if ($(this).prop('id') === 'story-nav') {
       $.smoothScroll({
         scrollTarget: $(this).attr('href').substr(1),
-        offset: -230,
+        offset: -200,
         speed: 1500
       });
     } 
@@ -42,40 +42,33 @@ $(document).ready(function(){
     var top = $(window).scrollTop();
     var bottom = top + $(window).height();
     var half = (bottom + top) / 2
-    var sponsorshipTop = $('#sponsorship').offset().top;
     var aboutTop = $('#about').offset().top;
-    var whyTop = $('#why-enganche').offset().top;
-    var storyTop = $('#our-story').offset().top;
+    var brandsTop = $('#brands').offset().top;
+    var sponsorshipTop = $('#sponsorship').offset().top;
+    var storyTop = $('#story').offset().top;
     var contactTop = $('#contact').offset().top;
 
     if (contactTop < half) {
       $(".navbar-enganche-link").removeClass("active");
       $('#contact-nav').addClass('active');
-    } else if (storyTop < half) {
-      $(".navbar-enganche-link").removeClass("active");
-      $('#story-nav').addClass('active');
-    } else if (whyTop < half) {
-      $(".navbar-enganche-link").removeClass("active");
-      $('#why-enganche-nav').addClass('active');
-    } else if (sponsorshipTop < half) {
-      $(".navbar-enganche-link").removeClass("active");
-      $('#sponsorship-nav').addClass('active');
     } else if (aboutTop < half) {
       $(".navbar-enganche-link").removeClass("active");
       $('#about-nav').addClass('active');
+    } else if (brandsTop < half) {
+      $(".navbar-enganche-link").removeClass("active");
+      $('#brands-nav').addClass('active');
+    } else if (sponsorshipTop < half) {
+      $(".navbar-enganche-link").removeClass("active");
+      $('#sponsorship-nav').addClass('active');
+    } else if (storyTop < half) {
+      $(".navbar-enganche-link").removeClass("active");
+      $('#story-nav').addClass('active');
       console.log('oi oi')
     } else {
       $(".navbar-enganche-link").removeClass("active");
     }
   });
-
-  // $(".arrow").click(function(){
-  //   $.smoothScroll({
-  //     scrollTarget: $(this).attr('href').substr(1),
-  //     offset: -230,
-  //     speed: 1000
-  //   });
-  // })
+//landing page buttons
   $(".brands-btn-landing").click(function(){
     $.smoothScroll({
       scrollTarget: $(this).attr('href').substr(1),
@@ -91,3 +84,10 @@ $(document).ready(function(){
     });
   })
 });
+// $(".arrow").click(function(){
+//   $.smoothScroll({
+//     scrollTarget: $(this).attr('href').substr(1),
+//     offset: -230,
+//     speed: 1000
+//   });
+// })
